faa9f79ba8ff8e737430c0ec766b3b4e27c1a85f
monamind <monamind.os@gmail.com>
2025-06-12 17:54:01 +0200

    Add real homepage component

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,9 @@
-// Hotová landing page MonaMind
+export default function Home() {
+  return (
+    <main style={{ padding: '3rem', fontFamily: 'Arial' }}>
+      <h1>Vítej na MonaMind</h1>
+      <p>Landing page je připravena. Web je funkční.</p>
+      <p><a href="/login">Přihlášení</a> | <a href="/register">Registrace</a></p>
+    </main>
+  );
+}
